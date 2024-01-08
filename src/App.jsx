adcb2d37cb3839796c030dfb2e0b1c23a8aebccd
abidnk <abidnk34@gmail.com>
2024-01-08 12:42:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Fornite from "./components/Fornite"
+
+
+
+function App() {
+
+  return (
+    <div>
+    <Fornite/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Fornite.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import "./Fornite.css";
+
+const Fornite = () => {
+  return (
+    <div className="task">
+      <img
+        className="cross"
+        alt=""
+        src="src/assets/line-md_menu-to-close-alt-transition.png"
+      />
+      <div className="epic-games">
+        Epic Games : An American video game and software developer and publisher
+        based in Cary, North Carolina.
+      </div>
+      <img
+        className="fortnite-pictures"
+        alt="imagr"
+        src="src/assets/fortnite-pictures-b5kfcchwazwiz3cs 1.png"
+      />
+      <div className="create-play">
+        Create, play, and battle with friends for free in Fortnite. Be the last
+        player standing in Battle Royale and Zero Build, experience a concert or
+        live event, or discover over a million creator made games, including
+        racing, parkour, zombie survival, and more. Each Fortnite island has an
+        individual age rating so you can find the one that's right for you and
+        your friends. Find it all in Fortnite ... Drop In.
+      </div>
+      <div className="task-inner">
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="visit-website">Visit Website</div>
+        </div>
+      </div>
+      <img className="mask-group-icon" alt="" src="src/assets/Mask group (1).png" />
+      <img className="mask-group-icon1" alt="" src="src/assets/Mask group (2).png" />
+      <img className="mask-group-icon2" alt="" src="src/assets/Mask group.png" />
+      <div className="explore-large-destructible">
+        Explore large, destructible environments where no two games are ever the
+        same.
+      </div>
+      <div className="team-up-with">
+        Team up with friends by sprinting, climbing and smashing your way to
+        earn your Victory Royale
+      </div>
+      <div className="discover-even-more">
+        Discover even more ways to play across thousands of creator-made game
+        genres
+      </div>
+      <div className="our-contribution-parent">
+        <div className="our-contribution">Our Contribution</div>
+        <div className="our-core-offering">
+          Our core offering extends beyond mere profit generation; we emphasize
+          the growth and active involvement of our user community. Collaborating
+          with us represents an investment, rather than a mere expenditure. Our
+          dedication to providing distinctive digital interactions guarantees
+          unparalleled benefits for our clientele.
+        </div>
+      </div>
+      <div className="group-parent">
+        <div className="m-parent">
+          <div className="m">5M</div>
+          <div className="daily-user-engagements-container">
+            <p className="daily-user">{`Daily User `}</p>
+            <p className="daily-user">
+              Engagements
+              <span className="span">{` `}</span>
+            </p>
+          </div>
+          <div className="revenue-surge-for-container">
+            <p className="daily-user">Revenue Surge for an</p>
+            <p className="daily-user">Platform</p>
+          </div>
+          <div className="roas-on-all-container">
+            <p className="daily-user">{`ROAS on all our `}</p>
+            <p className="daily-user">marketing campaigns</p>
+          </div>
+        </div>
+        <div className="k-wrapper">
+          <div className="k">$500K</div>
+        </div>
+        <div className="x-wrapper">
+          <div className="k">10X</div>
+        </div>
+      </div>
+      <div className="group-container">
+        <div className="interested-in-delving-deeper-i-parent">
+          <div className="interested-in-delving">
+            Interested in delving deeper into the project?
+          </div>
+          <div className="if-youd-like-container">
+            <span className="if-youd-like-container1">
+              <span className="if-youd-like">{`If you'd like to explore further details about our initiatives or any of our affiliated brands, don't hesitate to connect. You can reach out to us via email at `}</span>
+              <span className="helloabccom">hello@abc.com</span>
+              <span>{` `}</span>
+              <span className="if-youd-like">{`or give us a call at `}</span>
+              <span className="helloabccom">+91 480 20802730.</span>
+            </span>
+          </div>
+        </div>
+        <div className="rectangle-group">
+          <div className="group-item" />
+          <div className="contact-us">Contact Us</div>
+        </div>
+        <div className="rectangle-container">
+          <div className="frame-child" />
+          <div className="ring-us-on">Ring us on Skype</div>
+        </div>
+      </div>
+      <div className="abctechnology-solutions-pvt">
+        © 2019-2023 abcTechnology Solutions Pvt. Ltd. All Rights Reserved
+      </div>
+    </div>
+  );
+};
+
+export default Fornite;
